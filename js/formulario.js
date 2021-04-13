d5e0798208083d2296f20c5ef53cd9b29c144d31
JeisonPimentel <jeisonalvis10@gmail.com>
2021-04-13 01:09:02 -0400

    Se agrego componentes de Jquery al proyecto datapicket y select2

---
+++ js/formulario.js
@@ -0,0 +1,44 @@
+// - Constantes -
+const prevision = [ 
+    {idPrevision: "1", nombrePrevision: 'Fonasa'},
+    {idPrevision: "2", nombrePrevision: 'Isapre Cruz Blanca'},
+    {idPrevision: "3", nombrePrevision: 'Isapre Consalud'},
+]
+// - Fin de constantes -
+
+// - Inicio de (document).ready() -
+$(document).ready(function() {
+
+    console.log("hola mundo");
+    // - Select2 -
+    inicializarSelect2Prevision();
+
+    $('#btnReservarHora').click(function() { 
+        console.log('Hello');
+    });
+    // - Fin de Select2 -
+
+});
+// - Fin de (document).ready() -
+
+// - [Aquí realizaremos todos los metodos para crear los select2]
+function inicializarSelect2Prevision(){
+    // Iteramos el objeto global de servicio para agregar el campo id que requiere el control
+    var datos = $.map(prevision, function (obj) {
+        obj.id = obj.idPrevision;
+        obj.text =  obj.nombrePrevision;
+        return obj;
+    });
+
+    $('#tipoPrevision').select2(
+        {
+            placeholder: {id: "-1", text: "Seleccione una opción"},
+            data: datos,
+            allowClear: true
+        }
+    );
+};
+
+// - Funciones estaticas -
+// - [Espacio Reservado]
+// - Fin de funciones estaticas -
